2832007bfa31c973e0bf507a2fb629ddca73dbe9
Tomas Kozelek <tomas.kozelek@gmail.com>
2010-02-12 23:21:13 +0100

    basics of spoiled bridges functionality

--- Board.hs
+++ Board.hs
@@ -20,6 +20,10 @@ type Groups = Map.Map Pos Group
 
 data Board = Board {bdSize :: Size, bdPegs:: Pegs, bdToPlay :: Color, bdGroups :: Groups} 
 
+--abs + rel pos -> pos
+offsetPos :: Pos -> Pos -> Pos 
+offsetPos (posx, posy) (offx, offy) = (posx + offx, posy + offy)
+
 showSquare :: Board -> Pos -> String
 showSquare board pos = let value = Map.lookup pos (bdGroups board)
                        in case value of 
@@ -56,6 +60,38 @@ mkGroup peg = Group{
                     grpColor=pegColor peg,
                     grpPegs=[peg]
                     }
+
+swapPair :: (a,b) -> (b, a)
+swapPair (a, b) = (b, a)
+
+negateFirst :: (Num a) => (a, b) -> (a, b)
+negateFirst (a, b) = (-1 * a, b)
+
+--relative positions of cross pairs for bridge at referential position (2`oclock)
+relSpoilPairs = [((-2, 0), (0,  1)), ((-1, -1), (0, -1)), ((-1, 0), ( 1,  1)), (( 1, 0), (-1,  1)), 
+                 (( 2, 0), (0, -1)), (( 1,  1), (0, -1)), (( 1, 0), (-1, -1)), ((-1, 0), ( 1, -1))]
+
+--for a potential bridge (pair of positions) gives positions which can spoil it
+doGenSpoilPairs :: ((Pos, Pos) -> (Pos, Pos)) -> (Pos, Pos) -> [(Pos, Pos)]
+doGenSpoilPairs processor (pos1@(pos1x, pos1y), pos2@(pos2x, pos2y))
+    = map ((\(off1, off2) -> (offsetPos pos1 off1, offsetPos pos2 off2)) . processor) relSpoilPairs
+    -- = map (\((rpos1x, rpos1y), (rpos2x, rpos2y) -> ((rpos2x + pos2x), (rpos2y + pos2y))). processor) relSpoilPairs
+
+--wrapper around doGenSpoilPairs
+genSpoilPairs :: (Pos, Pos) -> [(Pos, Pos)]
+genSpoilPairs posPair@((pos1x, pos1y), (pos2x, pos2y))
+    --pos1 should be more on the left
+    | pos1x >= pos2x = genSpoilPairs ((pos2x, pos2y), (pos1x, pos1y))
+    --1 o`clock
+    | pos1y - pos2y == 2 = doGenSpoilPairs (\(pos1, pos2) -> (swapPair pos1, swapPair pos2)) posPair
+    --2 o`clock - referential position
+    | pos1y - pos2y == 1 = doGenSpoilPairs (\(pos1, pos2) -> (pos1, pos2)) posPair
+    --4 o`clock
+    | pos2y - pos1y == 1 = doGenSpoilPairs (\(pos1, pos2) -> (negateFirst pos1, pos2)) posPair
+    --5 o`clock
+    | pos2y - pos1y == 2 = doGenSpoilPairs (\(pos1, pos2) -> (swapPair (negateFirst pos1), swapPair pos2)) posPair
+    | otherwise = error "No matching pos pair position"
+
 
 relJumps :: [Pos]
 relJumps = [(1, -2), (1, 2), (2, 1), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
@@ -103,7 +139,6 @@ exportMaybes :: [Maybe a] -> [a]
 exportMaybes [] = [] 
 exportMaybes (Just x:xs) = x:(exportMaybes xs)
 exportMaybes (Nothing:xs) = exportMaybes xs
-
 
 placePeg :: Board -> Peg -> Board
 placePeg board peg = let newPegs = peg:(bdPegs board)
